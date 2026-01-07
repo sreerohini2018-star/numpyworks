8a1b49985d974440aa93872aa624d9a302cc3b2e
sreelakshmi <sreerohini2018@gmail.com>
2026-01-07 22:15:54 +0530

    numpy task

---
+++ slicing_practice.py
@@ -0,0 +1,32 @@
+
+"""
+slicing_practice:-
+
+         maths  english  malayalam  cs  social
+
+vipin     43      42        45      34   23
+
+hari      23      45        45      34   37
+
+cibin     37      38        39      40   45
+"""
+import numpy as np
+arr=np.array([
+    [43,42,45,34,23],
+    [23,45,45,34,37],
+    [37,38,39,40,45]
+])
+print(arr.ndim)
+print(arr.size)
+print(arr.shape)
+
+#display marks of hari
+print(arr[1])
+#display maths mark of hari
+print(arr[1,0])
+#display malayalam marks of all students
+print(arr[:,2])
+#display malayalam and cs marks of all students
+print(arr[:,2:4])
+#display english and malayalam marks of hari and cibin
+print(arr[1:3,1:3])
